beadbc56bad6e23b66ec7d2378a63a14c65113a9
vyshnavisathya <vyshnavisathya@users.noreply.github.com>
2020-08-19 12:00:36 +0530

    Add files via upload

---
+++ customerAPI/target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into customer values(1001, 'palamaner', 26, 'vyshnavi0693@gmail.com', 'vyshnavi', 8884250196);
+insert into customer values(1002, 'chittoor', 25, 'siri@gmail.com', 'siri', 9457586456);
+insert into customer values(1003, 'tirupati', 27, 'sree@gmail.com', 'sree', 7864676543);
+
+
