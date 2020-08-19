53fdd39414b4175616d3f7656a92457c04d3d576
vyshnavisathya <vyshnavisathya@users.noreply.github.com>
2020-08-19 12:07:50 +0530

    Added source code of eureka server

---
+++ serviceregistration/src/test/java/com/cognizant/serviceregistration/ServiceregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.serviceregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
